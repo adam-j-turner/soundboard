9bb1bc130ae26b547bff860e2fd53f1913e38043
Adam Turner <turner@BCPRD20BFY29-L.hrbinc.hrblock.net>
2019-05-17 10:56:13 -0500

    add editmode and GridLayout for editable buttons.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import RGL, { WidthProvider } from 'react-grid-layout';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import call_1 from './sounds/call_1.wav';
 import { Line } from 'rc-progress';
 import './App.css';
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
+
+const GridLayout = WidthProvider(RGL);
 
 class Container extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      progress: 0
+      progress: 0,
+      editMode: false
+      //isDraggable: false,
+      //isResizable: false,
     }
 
     this.interval = null;
+    this.changeEditMode = this.changeEditMode.bind(this)
     this.playSound = this.playSound.bind(this)
+  }
+
+  changeEditMode(value, event) {
+    this.setState({ editMode: value });
   }
 
   updateProgress() {
@@ -26,6 +41,9 @@ class Container extends React.Component {
   }
 
   playSound() {
+    // don't play sounds during edit mode
+    if (this.state.editMode) {return}
+
     console.log('play a sound');
     var audio = new Audio(call_1);
 
@@ -40,12 +58,27 @@ class Container extends React.Component {
   }
 
   render() {
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 3, h: 1, minW: 2, maxW: 4}
+    ];
+
     return (
       <div>
-        <Line percent={this.state.progress} />
-        <button onClick={this.playSound}>
-          Sound 1
-        </button>
+        <div className='header'>
+          <Line className='progressBar' percent={this.state.progress} />
+          <div className='editModeBar'>
+            <h1>Edit Mode: </h1>
+            <ToggleButtonGroup name="editMode" type="radio" onChange={this.changeEditMode}>
+              <ToggleButton type="radio" value={true}>ON</ToggleButton>
+              <ToggleButton type="radio" defaultChecked value={false} >OFF</ToggleButton>
+            </ToggleButtonGroup>
+          </div>
+        </div>
+        <GridLayout className="buttonGrid" layout={layout} cols={20} rowHeight={30} isDraggable={this.state.editMode} isResizable={this.state.editMode}>
+          <button key='a' onClick={this.playSound}>
+            Sound 1
+          </button>
+        </GridLayout>
       </div>
     )
   }
