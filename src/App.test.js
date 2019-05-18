09561bcede2748651cfa4ae9cb98edf5f88bb1fa
CollinEMac <macd.collin@gmail.com>
2019-05-18 13:49:54 -0500

    Remove some unused css classes

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// TODO: Create actual functionality tests
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
