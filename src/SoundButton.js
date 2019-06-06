e5a87d9431a9b694a07a4c8046a226e19785213f
Adam Turner <turner@BCPRD20BFY29-L.hrbinc.hrblock.net>
2019-06-06 10:39:37 -0500

    Convert SoundButton component to material-ui Button.

--- src/SoundButton.js
+++ src/SoundButton.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid'
+import Button from "@material-ui/core/Button";
+// TODO: Have Tooltip show sound description on hover.
+// import Tooltip from '@material-ui/core/Tooltip';
 
 class SoundButton extends React.Component {
   constructor(props){
@@ -8,14 +12,20 @@ class SoundButton extends React.Component {
 
   render() {
     return (
-      <button
-        {...this.props}
-        className='button'
-        key={this.props.key}
-        onClick={this.props.onClick}
-      >
-        {this.props.text}
-      </button>
+      <Grid item key={this.props.key}>
+        <Button
+          {...this.props}
+          className='Button-base SoundButton'
+          aria-controls="customized-menu"
+          aria-haspopup="true"
+          variant="contained"
+          color="primary"
+          key={this.props.key}
+          onClick={() => {this.props.onClick(this.props.audioPath)}}
+        >
+          {this.props.text}
+        </Button>
+      </Grid>
     )
   }
 }
